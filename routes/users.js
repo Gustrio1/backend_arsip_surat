59f1edca8ab7aa17e67d3ac5244c81f3ee532e39
Gustrio1 <133954459+Gustrio1@users.noreply.github.com>
2025-06-16 01:38:02 +0800

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const database = require("../db");
+require("dotenv").config();
 
 router.post("/login", async (req, res) => {
   const { username, password } = req.body;
